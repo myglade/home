233939118967c283c867633c71f4fc5bbe7f833e
Heesung Kim <myglade@gmail.com>
2017-01-23 23:21:23 -0800

    1/22 home

--- db.py
+++ db.py
@@ -1,16 +1,33 @@
+import logging
 import sqlite3
+
+log = logging.getLogger(__name__)
 
 class Db(object):
     """ database """
-    def __init__(self, name):
-        pass
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.conn = sqlite3.connect(db_file)
+        # except lite.Error, e:
 
-    def _create_table(name, columns):
-        pass
+    def execute(self, sql):
+        log.debug("execute: %s", sql)
+        return self.conn.execute(sql)
 
-    def create_image_table():
-        pass
+    def insert(self, table, data):
+        """
+        table (str) : table name
+        data (dict) : values
+        """
 
-    def create_media_table():
-        pass
+        sql = ""
+        key = ""
+        values = ""
+        param = ()
+        for col, val in data.iteritems():
+            key += "," + col
+            values += ",?"
+            param += (val)
 
+        sql = "INSERT INTO %s (%s) VALUES(%s)"
+        self.conn.execute(sql)
