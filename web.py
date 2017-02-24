0cb771c3e73cd38ed982c16a40d905bd838d3caa
Heesung Kim <myglade@gmail.com>
2017-02-24 00:57:59 -0800

    2/23 home

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
 from flask import (
     Flask, 
     request,
-    jsonify
+    jsonify,
+    send_file
     )
+import os.path
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 import image_manager
 import json
@@ -14,13 +16,16 @@ import logging
 def hello():
     return "Hello World!"
 
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
 @app.route("/nextimage")
 def next_image():
-
-    a = request.args
     id = request.args.get('id')
     img = image_manager.get_newimage(id)
     return jsonify(img)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG,
